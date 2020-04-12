e2ff82ba46f8294496a6437c39ba6e8ab8bb44d7
Hana <momenthana@kakao.com>
2020-04-12 11:13:19 +0900

    export 구조 수정

---
+++ lib/func/index.ts
@@ -0,0 +1,13 @@
+import classroom from './classroom'
+import major from './major'
+import meal from './meal'
+import schedule from './schedule'
+import search from './search'
+
+export {
+  classroom,
+  major,
+  meal,
+  schedule,
+  search
+}
