c6f259b36fea2f8634106a4ee8f945a21ed77760
Hana <momenthana@kakao.com>
2020-04-13 12:15:42 +0900

    console 순차 출력

--- test/index.js
+++ test/index.js
@@ -3,22 +3,22 @@ const school = require('../dist')
 school.search({
   SCHUL_NM: '하나고등학교'
 })
-  .then(res => {
-    console.log(res[0])
-    school.meal(res[0])
+  .then(async res => {
+    console.log('SEARCH:', res[0])
+    await school.meal(res[0])
       .then(res => {
-        console.log(res[0])
+        console.log('MEAL:', res[0])
       })
-    school.schedule(res[0])
+    await school.schedule(res[0])
       .then(res => {
-        console.log(res[0])
+        console.log('SCHEDULE:', res[0])
       })
-    school.major(res[0])
+    await school.major(res[0])
       .then(res => {
-        console.log(res[0])
+        console.log('MAJOR:', res[0])
       })
-    school.classroom(res[0])
+    await school.classroom(res[0])
       .then(res => {
-        console.log(res[0])
+        console.log('CLASSROOM:', res[0])
       })
   })
