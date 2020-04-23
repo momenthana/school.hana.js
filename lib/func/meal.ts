1d782aa177c4d7c56c60ce2fe1b2b1a7fabdf732
Hana <momenthana@kakao.com>
2020-04-23 20:00:18 +0900

    급식 기준일 기본값 추가

--- lib/func/meal.ts
+++ lib/func/meal.ts
@@ -1,9 +1,12 @@
 import axios from 'axios'
 
 const meal = (object: object) => {
+  const date = new Date()
+
   return axios.get('https://open.neis.go.kr/hub/mealServiceDietInfo', {
     params: Object.assign({
       Type: 'json',
+      MLSV_YMD: date.getFullYear().toString() + (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate())
     }, object)
   })
     .then(req => {
