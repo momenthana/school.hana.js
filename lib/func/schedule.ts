89990372777b304d209e5d3d0535bf771032afc2
Hana <momenthana@kakao.com>
2020-04-24 14:27:03 +0900

    학사일정 기준달 기본값 추가

--- lib/func/schedule.ts
+++ lib/func/schedule.ts
@@ -1,9 +1,16 @@
 import axios from 'axios'
 
 const schedule = (object: object) => {
+  const date = new Date()
+  const FROM_YM = date.getFullYear().toString() + (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1)
+  date.setMonth(date.getMonth() + 1)
+  date.setDate(0)
+
   return axios.get('https://open.neis.go.kr/hub/SchoolSchedule', {
     params: Object.assign({
       Type: 'json',
+      AA_FROM_YMD: FROM_YM + '01',
+      AA_TO_YMD: FROM_YM + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate())
     }, object)
   })
     .then(req => {
