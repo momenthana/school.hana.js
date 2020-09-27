e04ba7c71d8443f1ca2b188beea13178fddc4cfb
Hana <momenthana@kakao.com>
2020-09-28 00:29:19 +0900

    Release 1.0.0

--- test/index.js
+++ test/index.js
@@ -2,31 +2,36 @@ const school = require('../dist')
 
 school.search({
   SCHUL_NM: '하나고등학교'
+}).then(async res => {
+  console.log('SEARCH:', res[0])
+
+  await school.meal(res[0])
+    .then(res => {
+      console.log('MEAL:', res[0])
+    })
+
+  await school.schedule(res[0])
+    .then(res => {
+      console.log('SCHEDULE:', res[0])
+    })
+
+  await school.major(res[0])
+    .then(res => {
+      console.log('MAJOR:', res[0])
+    })
+
+  await school.classes(res[0])
+    .then(res => {
+      console.log('CLASSES:', res[0])
+    })
+
+  await school.timetable(res[0])
+    .then(res => {
+      console.log('TIMETABLE:', res[0])
+    })
+
+  await school.classroom(res[0])
+    .then(res => {
+      console.log('CLASSROOM:', res[0])
+    })
 })
-  .then(async res => {
-    console.log('SEARCH:', res[0])
-    await school.meal(res[0])
-      .then(res => {
-        console.log('MEAL:', res[0])
-      })
-    await school.schedule(res[0])
-      .then(res => {
-        console.log('SCHEDULE:', res[0])
-      })
-    await school.major(res[0])
-      .then(res => {
-        console.log('MAJOR:', res[0])
-      })
-    await school.classes(res[0])
-      .then(res => {
-        console.log('CLASSES:', res[0])
-      })
-    await school.timetable(res[0])
-      .then(res => {
-        console.log('TIMETABLE:', res[0])
-      })
-    await school.classroom(res[0])
-      .then(res => {
-        console.log('CLASSROOM:', res[0])
-      })
-  })
